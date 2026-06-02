bb61c7102db281629b86a02bf5a77a07f49d5932
riikkayoki <74421508+riikkayoki@users.noreply.github.com>
2026-06-02 13:51:26 +0300

    8.6

--- library-backend/index.js
+++ library-backend/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("@apollo/server")
 const { startStandaloneServer } = require("@apollo/server/standalone")
+const { v1: uuid } = require("uuid")
 
 let authors = [
   {
@@ -115,6 +116,15 @@ const typeDefs = `
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book!
+  }
 `
 
 const resolvers = {
@@ -130,6 +140,17 @@ const resolvers = {
   Author: {
     bookCount: (root) =>
       books.filter((book) => book.author === root.name).length,
+  },
+  Mutation: {
+    addBook: (_, args) => {
+      if (!authors.find((author) => author.name === args.author)) {
+        authors.push({ name: args.author, id: uuid() })
+      }
+
+      const book = { ...args, id: uuid() }
+      books.push(book)
+      return book
+    },
   },
 }
 
