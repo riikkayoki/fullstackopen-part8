265f13bf944b19415ba2712da0006e823389c9ff
riikkayoki <74421508+riikkayoki@users.noreply.github.com>
2026-06-02 13:38:01 +0300

    8.5

--- library-backend/index.js
+++ library-backend/index.js
@@ -112,7 +112,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -122,9 +122,9 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (_, args) =>
-      args.author
-        ? books.filter((book) => book.author === args.author)
-        : books,
+      books
+        .filter((book) => !args.author || book.author === args.author)
+        .filter((book) => !args.genre || book.genres.includes(args.genre)),
     allAuthors: () => authors,
   },
   Author: {
